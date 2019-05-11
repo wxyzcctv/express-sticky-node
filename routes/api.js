b6fa79c6f2aaba2e9179263df45e77f75daf3799
wxyzcctv <2846629226@qq.com>
2019-05-11 11:24:55 +0800

    实现便利贴的增删改

--- routes/api.js
+++ routes/api.js
@@ -10,13 +10,30 @@ router.get('/notes', function(req, res, next) {
 });
 router.post('/notes/add',function(req, res, next){
   var note = req.body.note
+  Note.create({text:note}).then(()=>{
+    res.send({status:0})
+  }).catch(()=>{
+    res.send({status: 1, errorMsg: '添加出错'})
+  })
   console.log('add..', note)
 });
 router.post('/notes/edit',function(req, res, next){
-
+  Note.update({text: req.body.note}, {where: {id: req.body.id}} ).then(()=>{
+    console.log(arguments)
+    res.send({status: 0})
+  }).catch(()=>{
+      res.send({status: 1, errorMsg: '更新失败'})
+    }
+  )
 });
 router.post('/notes/delete',function(req, res, next){
-
+  Note.destroy({where:{id:req.body.id}}).then(()=>{
+    res.send({status: 0})
+    console.log('false')
+  }).catch(()=>{
+      res.send({status: 1, errorMsg: '删除出错'})
+    }
+  )
 })
 
 module.exports = router;
