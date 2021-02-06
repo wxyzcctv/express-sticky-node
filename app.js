ee8508e7d9c404d3c454d988dd01d7acd2e1761c
wxyzcctv <2846629226@qq.com>
2021-02-06 09:03:11 +0800

    初始化分支study-1

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser')
-var passport = require('passport')
-var session = require('express-session');
+var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var auth = require('./routes/auth')
-var api = require('./routes/api');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -18,25 +14,21 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'sessionsecret'}));
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', indexRouter);
-app.use('/auth',auth)
-app.use('/api', api);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var loginData;
-  if(req.session.user){
-    loginData = {
-      isLogin: true,
-      user:{
-        avatar: req.session.user.avatar,
-        username: req.session.user.username
-      }
-    }
-  }else{
-    loginData = {
-      isLogin: false
-    }
-  }
-  res.render('index', loginData);
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
